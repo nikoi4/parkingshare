f0d18cdc4b1078537153c8c0039c7e2de1eb410c
Nicolas Caif <r.nicolascaif@gmail.com>
2019-02-25 17:53:56 -0300

    validations settled for models

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -5,4 +5,11 @@ class Parking < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings
   has_many :users, through: :bookings
+
+  validates :name, precense: true
+  validates :address, precense: true, uniqueness: { scope: :name }
+  validates :picture, precense: true
+  validates :description, length: { minimum: 20 }
+  validates :size, inclusion: { in: ["Small Car", "Medium Car", "Big Car", "Truck"] }
+  validates :price_cents, precense: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :booking
+
+  validates :content, precense: true
+  validates :rating, inclusion: { in: [1, 2, 3, 4, 5] }
+  validates :date, presence: true
 end
