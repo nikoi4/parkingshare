3c4279f14964b1e3aefbd2e225d4e5a8f6af8ba9
Nicolas Caif <r.nicolascaif@gmail.com>
2019-02-25 15:56:32 -0300

    initial commit

---
+++ db/migrate/20190225185525_create_parkings.rb
@@ -0,0 +1,18 @@
+class CreateParkings < ActiveRecord::Migration[5.2]
+  def change
+    create_table :parkings do |t|
+      t.string :description
+      t.string :parking
+      t.string :name
+      t.string :address
+      t.float :latitude
+      t.float :longitude
+      t.string :picture
+      t.string :size
+      t.string :price_cents
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_25_181433) do
+ActiveRecord::Schema.define(version: 2019_02_25_185108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "descriptions", force: :cascade do |t|
+    t.string "parking"
+    t.string "name"
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "picture"
+    t.string "size"
+    t.string "price_cents"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_descriptions_on_user_id"
+  end
+
+  create_table "features", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +49,5 @@ ActiveRecord::Schema.define(version: 2019_02_25_181433) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "descriptions", "users"
 end
