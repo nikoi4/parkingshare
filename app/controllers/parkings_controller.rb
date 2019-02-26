0b37fd8ca6e329918441cb46f71e1e252d5ac6cf
Nicolas Caif <r.nicolascaif@gmail.com>
2019-02-26 20:18:16 -0300

    work in progress

--- app/controllers/parkings_controller.rb
+++ app/controllers/parkings_controller.rb
@@ -1,6 +1,6 @@
 class ParkingsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :new]
-  before_action :set_parking, only: [:create, :update, :destroy]
+  before_action :set_parking, only: [:update, :destroy]
   def index
     @parkings = policy_scope(Parking)
   end
@@ -12,19 +12,38 @@ class ParkingsController < ApplicationController
   end
 
   def create
+    @parking = Parking.new(parking_params)
+    # @features = array??
     authorize @parking
+    if @parking.save
+      @parking_lot_features = @features.each do |feature|
+        plf = ParkingLotFeatures.new
+        plf.feature = feature.first
+        plf.value = feature.last
+        plf.parking = @parking
+        plf.save
+      end
+      # redirect_to ...
+    else
+      render :new
+    end
   end
 
   def update
   end
 
   def destroy
+    if @parking.destroy
+    # redirect_to ...
+    else
+      # render :
+    end
   end
 
   private
 
   def parking_params
-    params.require(:parking).permit(:name, :address, :picture, :size, :description, :pice_cents)
+    params.require(:parking).permit(:name, :address, :picture, :size, :description, :pice_cents, :features)
   end
 
   def set_parking
