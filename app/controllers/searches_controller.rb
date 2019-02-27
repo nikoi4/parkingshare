2f3bafdcfcb59118f48441a6bfa86fb7dadc3251
meli fernandez sassone <mfernandezsassone@gmail.com>
2019-02-27 20:37:18 -0300

    search data home no lindo

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,8 +1,10 @@
 class SearchesController < ApplicationController
+  skip_before_action :authenticate_user!
 
   def create
     @search = Search.new(search_params)
     @search.user = current_user if user_signed_in?
+    authorize @search
     @search.save
     redirect_to parkings_path(
       address: search_params[:address],
