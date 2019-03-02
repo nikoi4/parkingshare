4aaadf65e3194661f467bb649de5e0c0315ef580
meli fernandez sassone <mfernandezsassone@gmail.com>
2019-03-02 12:35:37 -0300

    log in done

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,6 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-<<<<<<< HEAD
     strings: ["If it fits,", "it sits"],
     typeSpeed: 50,
     loop: true
