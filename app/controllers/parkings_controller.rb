3a5da9d9cfb1bcc8e1f8978860dc4ee3d513e7fd
rodrigo cabral <cabral-rodrigo@hotmail.com>
2019-03-06 10:10:47 -0300

    to see

--- app/controllers/parkings_controller.rb
+++ app/controllers/parkings_controller.rb
@@ -4,13 +4,10 @@ class ParkingsController < ApplicationController
 
   def index
     @dates = [params[:starting], params[:ending]]
-<<<<<<< HEAD
     @lat_long = params[:lat_long]
-    @parkings = Parking.available(@dates.first, @dates.last, @lat_long)
-=======
     @feature_ids = params[:feature_ids]
-    @parkings = policy_scope(Parking).where("features.id = ?", params[:feature_ids])
->>>>>>> 3bc869f64d6fd8d2dd1f320f6e456333fceb2494
+    @parkings = Parking.available(@dates.first, @dates.last, @lat_long)
+    # @parkings = policy_scope(Parking).where("features.id = ?", params[:feature_ids])
     raise
     @parkings = policy_scope(Parking).near(params[:lat_long], 50)
     @features = Feature.all
