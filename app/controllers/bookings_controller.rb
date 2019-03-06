f87473397d591260fd11f95edd316b151a8aa151
rodrigo cabral <cabral-rodrigo@hotmail.com>
2019-03-05 21:15:16 -0300

    format done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,8 +3,8 @@ class BookingsController < ApplicationController
   before_action :set_parking, only: [:new, :create]
   before_action :set_features, only: [:new, :show]
   def index
-    @owned_bookings = current_user.owned_bookings
-    @booked_bookings = current_user.bookings
+    @owned_bookings = current_user.owned_bookings.sort_by { |booking| booking.updated_at }.reverse
+    @booked_bookings = current_user.bookings.sort_by { |booking| booking.updated_at }.reverse
 
     skip_policy_scope
   end
