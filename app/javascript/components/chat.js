d10dd9a98782b1fbfa292a7477370e11f9cba0fe
meli fernandez sassone <mfernandezsassone@gmail.com>
2019-03-07 12:46:44 -0300

    chat timestamp

--- app/javascript/components/chat.js
+++ app/javascript/components/chat.js
@@ -1,13 +1,19 @@
 $(document).ready(() => {
   let username = '';
 
+
   const updateChat = (data, sender) => {
+    const formatDate = (date) => {
+      return `${date.getDay()}-${date.getMonth()}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`;
+    };
     $('.chat-box').append(`
       <div class="chat-bubble-wrapper">
         <div class="chat-bubble ${sender}">
           <div class="chat-username">${data.identifier}</div>
           <div class="chat-message">${data.message}</div>
         </div>
+
+        <div class="chat-timestamp  ${sender}">${formatDate(new Date(data.created_at))}</div>
       </div>
     `);
   };
