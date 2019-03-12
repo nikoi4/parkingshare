58b1610a2cb3e19208b7fc42cbcd9fdc5a418578
R. Nicolas Caif <r.nicolascaif@gmail.com>
2019-03-11 21:38:21 -0300

    Revert "testing"

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -4,6 +4,6 @@ import "flatpickr/dist/themes/airbnb.css" // A path to the theme CSS
 
 flatpickr(".datepicker", {
     enableTime: true,
-    dateFormat: "M-d H:i",
-    minDate: "Mar-08 21:02",
+    minDate: "today",
+    dateFormat: "M-d H:i"
 })
