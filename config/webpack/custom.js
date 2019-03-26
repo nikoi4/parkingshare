7abacdf05d3c0655a3a4fe6068150a7f922ee424
R. Nicolas Caif <r.nicolascaif@gmail.com>
2019-03-26 17:27:00 -0300

    Revert "trying to fix minified code outside of NODE_ENV === 'production'"

--- config/webpack/custom.js
+++ config/webpack/custom.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 module.exports = {
    plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     }),
   ]
 };
